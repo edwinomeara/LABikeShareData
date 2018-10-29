d88b17895eef663741ff04896240cd386c4f3b5a
Edwin O'Meara <omearaedwin@yahoo.com>
2018-10-28 20:40:44 -0400

    Update create-graph.js

--- javascripts/create-graph.js
+++ javascripts/create-graph.js
@@ -10,7 +10,8 @@
 //parse data from the csv file using PapaParse and create graphs needed.
 function parseData(createGraph){
    // Papa.parse("https://edwinomeara.github.io/LABikeShareData/dataAndImages/metrobikesharetripdata.csv", {
-   Papa.parse("/dataAndImages/metrobikesharetripdata.csv", {
+	Papa.parse("https://media.githubusercontent.com/media/edwinomeara/LABikeShareData/master/dataAndImages/metrobikesharetripdata.csv", {
+   //Papa.parse("/dataAndImages/metrobikesharetripdata.csv", {
       download: true,
       complete: function(results) {
 	 setTimeout(function(){createGraph(results.data) ; }, 5000);
